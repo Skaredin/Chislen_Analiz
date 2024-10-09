e3ed9fbafef31c2f8ea925e03a3b9f688cf6d58f
Skaredin <sksv94@mail.ru>
2024-10-09 12:34:41 +0500

    Сделал график

--- Chislen_Analiz/Chapter-3.xaml.cs
+++ Chislen_Analiz/Chapter-3.xaml.cs
@@ -7,34 +7,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Chislen_Analiz
 {
-	/// <summary>
-	/// Логика взаимодействия для Chapter_3.xaml
-	/// </summary>
 	public partial class Chapter_3 : Window
 	{
 		public Chapter_3()
 		{
 			InitializeComponent();
 		}
+
 		private void CalculateLagrange_Click(object sender, RoutedEventArgs e)
 		{
 			try
 			{
 				var (xValues, yValues) = ParseInput(PointsInput.Text);
-				double result = LagrangeInterpolation.Interpolate(xValues, yValues, 2);  // Пример с x = 2
+				List<CalculationStep> steps;
+				double result = LagrangeInterpolation.Interpolate(xValues, yValues, 2, out steps);  // Пример с x = 2
 				LagrangeResult.Text = result.ToString();
 
+				// Заполняем таблицу шагами
+				StepsDataGrid.ItemsSource = steps;
+
 				// Отобразить график для Лагранжа
-				PlotInterpolation(xValues, yValues, LagrangeInterpolation.Interpolate, "Полином Лагранжа");
+				PlotInterpolation(xValues, yValues, (xVals, yVals, x) => LagrangeInterpolation.Interpolate(xVals, yVals, x, out steps), "Полином Лагранжа", OxyColors.Blue);
 			}
 			catch (Exception ex)
 			{
@@ -47,11 +43,15 @@ namespace Chislen_Analiz
 			try
 			{
 				var (xValues, yValues) = ParseInput(PointsInput.Text);
-				double result = NewtonInterpolation.Interpolate(xValues, yValues, 2);  // Пример с x = 2
+				List<CalculationStep> steps;
+				double result = NewtonInterpolation.Interpolate(xValues, yValues, 2, out steps);  // Пример с x = 2
 				NewtonResult.Text = result.ToString();
 
+				// Заполняем таблицу шагами
+				StepsDataGrid.ItemsSource = steps;
+
 				// Отобразить график для Ньютона
-				PlotInterpolation(xValues, yValues, NewtonInterpolation.Interpolate, "Полином Ньютона");
+				PlotInterpolation(xValues, yValues, (xVals, yVals, x) => NewtonInterpolation.Interpolate(xVals, yVals, x, out steps), "Полином Ньютона", OxyColors.Red);
 			}
 			catch (Exception ex)
 			{
@@ -105,7 +105,7 @@ namespace Chislen_Analiz
 			return (xValues, yValues);
 		}
 
-		private void PlotInterpolation(List<double> xValues, List<double> yValues, Func<List<double>, List<double>, double, double> interpolate, string title)
+		private void PlotInterpolation(List<double> xValues, List<double> yValues, Func<List<double>, List<double>, double, double> interpolate, string title, OxyColor lineColor)
 		{
 			var plotModel = new PlotModel { Title = title };
 
@@ -118,7 +118,7 @@ namespace Chislen_Analiz
 			plotModel.Series.Add(scatterSeries);
 
 			// Добавляем интерполяционную кривую
-			var lineSeries = new LineSeries();
+			var lineSeries = new LineSeries { Color = lineColor };
 			double step = (xValues[xValues.Count - 1] - xValues[0]) / 100.0;
 			for (double x = xValues[0]; x <= xValues[xValues.Count - 1]; x += step)
 			{
@@ -128,5 +128,88 @@ namespace Chislen_Analiz
 
 			PlotView.Model = plotModel;
 		}
+	}
+
+	public static class LagrangeInterpolation
+	{
+		public static double Interpolate(List<double> xValues, List<double> yValues, double x, out List<CalculationStep> steps)
+		{
+			steps = new List<CalculationStep>(); // Заполните шаги
+			double result = 0;
+
+			for (int i = 0; i < xValues.Count; i++)
+			{
+				double term = yValues[i];
+				string stepDescription = $"L({i}) = {yValues[i]}";
+				for (int j = 0; j < xValues.Count; j++)
+				{
+					if (i != j)
+					{
+						term *= (x - xValues[j]) / (xValues[i] - xValues[j]);
+						stepDescription += $" * (({x} - {xValues[j]}) / ({xValues[i]} - {xValues[j]}))";
+					}
+				}
+				result += term;
+				stepDescription += $" = {term}";
+				steps.Add(new CalculationStep { Step = stepDescription, Result = term });
+			}
+			return result;
+		}
+	}
+
+	public static class NewtonInterpolation
+	{
+		public static double Interpolate(List<double> xValues, List<double> yValues, double x, out List<CalculationStep> steps)
+		{
+			steps = new List<CalculationStep>();
+			int n = xValues.Count;
+			double[] dividedDifferences = new double[n];
+
+			// Заполнение массива значениями y
+			for (int i = 0; i < n; i++)
+			{
+				dividedDifferences[i] = yValues[i];
+			}
+
+			// Вычисление разделенных разностей
+			for (int j = 1; j < n; j++)
+			{
+				for (int i = n - 1; i >= j; i--)
+				{
+					dividedDifferences[i] = (dividedDifferences[i] - dividedDifferences[i - 1]) / (xValues[i] - xValues[i - j]);
+					// Сохраняем шаги вычисления
+					steps.Add(new CalculationStep
+					{
+						Step = $"f[{i},{j}] = (f[{i}] - f[{i - 1}]) / ({xValues[i]} - {xValues[i - j]})",
+						Result = dividedDifferences[i]
+					});
+				}
+			}
+
+			// Вычисление значения интерполяционного многочлена
+			double result = dividedDifferences[0];
+			double term = 1;
+
+			for (int i = 1; i < n; i++)
+			{
+				term *= (x - xValues[i - 1]);
+				result += dividedDifferences[i] * term;
+
+				// Сохраняем шаги вычисления
+				steps.Add(new CalculationStep
+				{
+					Step = $"P({x}) += f[{i}] * (x - x[{i - 1}])",
+					Result = result
+				});
+			}
+
+			return result;
+		}
+	}
+
+	public class CalculationStep
+	{
+		public string Step { get; set; }
+		public double Result { get; set; }
 	}
 }
